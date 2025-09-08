457eebe7f9acc67a29266a45e406fbc35f4da6a6
Jack Fetkovich <johnfetkovich1014@gmail.com>
2025-09-08 12:45:53 -0400

    moving obstacle engulfs dot

--- unicyle/unicyle_mppi.py
+++ unicyle/unicyle_mppi.py
@@ -18,7 +18,7 @@ params = Parameters(
     max_w = 10.0, # max angular velocity (radians/s)
     max_v_dot = 8.0, # max linear acceleration (m/s^2)
     max_w_dot = 30.0, # max angular acceleration (radians/s^2) (8.0)
-    obstacles = np.array([[3.85, 3.8, 0.5], [2.0, 0.1, 0.25]])
+    obstacles = np.array([[3.85, 3.8, 0.1]])
 )
 
 # Main function
@@ -64,36 +64,66 @@ def main():
         (2.5, 1.0)
     ]
 
-    # Compute forward tangents
-    headings = []
-    for i in range(len(points)):
-        if i < len(points)-1:
-            dx = points[i+1][0] - points[i][0]
-            dy = points[i+1][1] - points[i][1]
-            headings.append(np.arctan2(dy, dx))
-        else:
-            headings.append(headings[-1])  # reuse last heading for final point
+    obstacle_points = [
+        (4.0, 2.0),
+        (4.5, 2.0),
+        (3.0, 3.5),
+        (3.5, 3.0),
+        (3.0, 2.5),
+        (3.0, 1.5),
+        (3.0, 0.9), # Keep Same
+        (2.5, 0.6),
+        (3.7, 2.0),
+        (3.85, 2.6),
+        (3.9, 3.2),
+        (3.85, 3.8),
+        (3.7, 4.3),
+        (3.4, 4.8),
+        (3.0, 5.1), # Keep Same
+        (2.5, 2.5),
+        (2.0, 2.5),
+        (1.5, 2.5),
+        (0.75, 4.5),
+        (0.5, 5.1),
+        (0.1, 4.8), # Keep Same
+        (0.2, 3.6),
+        (0.4, 3.8),
+        (-1, 3.5),
+        (-1, 3.2),
+        (-0.5, 2.5),
+        (-0.2, 2.0), # Keep Same
+        (3.0, 3.0),
+        (3.0, 3.0),
+        (3.0, 3.0),
+        (3.0, 3.0),
+        (3.0, 3.0),
+        (3.0, 3.0)
+    ]
 
-    # Unwrap for continuity
-    headings_unwrapped = np.unwrap(headings)
-
-    # Combine into (x, y, theta)
-    waypoints = [(p[0], p[1], th) for p, th in zip(points, headings_unwrapped)]
-    
+    ### Zeroed arrays used for calcuation
     Tx = int(distance_of_path(np.array(points)) / (params.max_v*0.2941176*params.dt))
     x = np.array([0,0,0,0,0])  # Initial state [x, theta, x_dot, theta_dot] -- tracks current state
     X = np.zeros((Tx, 5)) # list of historical states
     U = np.zeros((Tx, 2)) # list of historical control inputs
     all_weights = np.zeros((Tx, params.K)) # Weights of every generated trajectory, organized by time step
     costs = np.zeros(Tx)
-    traj = generate_trajectory_from_waypoints(waypoints, Tx) # trajectory of waypoints
-    # traj[:, 2] = new_thetas
-    # np.savetxt('trajectory.csv', traj, delimiter=',', fmt='%.4f')
-
     sample_trajectories = np.zeros((Tx, params.K, 3, params.T))
     sample_trajectories_one = np.zeros((params.K, 3, params.T)) # k sets of (x1, x2, ..., xn), (y1, y2, ..., yn), (w1, w2, ..., wn)
-    last_u = np.zeros(2) # the control input from the previous iteration
-    for t in range(Tx-1): # From 0 -> 159
+    last_u = np.zeros(2) # the control input from the previous 
+    
+    x_ob = np.zeros(Tx)
+    y_ob = np.zeros(Tx)
+    ## Zeroed arrays used for calculation
+    
+    ## Generation of waypoints for obstacle and robot
+    traj = generate_trajectory_from_waypoints(points, Tx) # trajectory of waypoints
+    obstacle_traj = generate_trajectory_x_y(obstacle_points, Tx) # Moving obstacle trajectory
+
+    
+    for t in range(Tx-1):
+        params.obstacles[0] = np.array([obstacle_traj[t, 0], obstacle_traj[t,1], params.obstacles[0,2]])
+        x_ob[t] = params.obstacles[0][0]
+        y_ob[t] = params.obstacles[0][1]
         u_nom, X_calc, traj_weight_single = mppi(x, last_u, traj[t+1: min(t+1+params.T, len(traj))], params) # Calculate the optimal control input
         # U[t] = safety_filter(u_nom, x)
         U[t] = u_nom
@@ -125,7 +155,7 @@ def main():
     #         writer.writerow(np.array([t,traj[t][0], traj[t][1], traj[t][2],X[t][0], X[t][1], X[t][2], costs[t]]))
 
 
-    animate(x_pos, y_pos, traj[:, 0], traj[:, 1], sample_trajectories, all_weights, params)
+    animate(x_pos, y_pos, traj[:, 0], traj[:, 1], x_ob, y_ob, sample_trajectories, all_weights, params)
 
 
 if __name__ == "__main__":
