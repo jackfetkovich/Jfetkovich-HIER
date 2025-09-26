704784ae74e0cfcf9b7e49728262e6cc4bc24176
Jack Fetkovich <johnfetkovich1014@gmail.com>
2025-09-26 00:27:34 -0400

    avoiding obstacles

--- unicyle/unicyle_mppi.py
+++ unicyle/unicyle_mppi.py
@@ -12,18 +12,18 @@ import matplotlib.pyplot as plt
 params = Parameters(
     dt = 0.025, # time step for MPPI
     safety_dt = 0.001, # time step for safety
-    K = 1000,   # number of samples
-    T = 15, # time steps (HORIZON)
+    K = 2000,   # number of samples
+    T = 20, # time steps (HORIZON)
     sigma = 2,
     lambda_ = 2,
-    l = 0.1,
+    l = 0.3,
     r = 0.1, 
     max_v = 6.0, # max x velocity (m/s)
-    max_w = 15.0, # max angular velocity (radians/s)
+    max_w = 20.0, # max angular velocity (radians/s)
     max_v_dot = 8.0, # max linear acceleration (m/s^2)
     max_w_dot = 30.0, # max angular acceleration (radians/s^2) (8.0)
-    obstacles = np.array([(4, 0, 0.2)]),
-    last_obstacle_pos = np.array([[0.0, 0.0]]),
+    obstacles = np.array([(4, 0, 0.2), (6, 0, 0.2) ]),
+    last_obstacle_pos = np.array([[0.0, 0.0], [0.0, 0.0]]),
     first_filter = True
 )
 
@@ -42,6 +42,10 @@ def main():
     obstacle_points = [ 
     [
         (4, 0), 
+        (2, 0)
+    ],
+    [
+        (6, 0), 
         (2, 0)
     ]
     ]
