b0a15cd3ef4025c534f33a6a8287848d819dfed6
Jack Fetkovich <johnfetkovich1014@gmail.com>
2025-11-07 09:32:51 -0500

    checkpoint

--- unicyle/plot_opt.py
+++ unicyle/plot_opt.py
@@ -6,7 +6,7 @@ comp_time_list = []
 opt_list = []
 
 file_names = [
-    'rollout_filter_1200_count_opt.csv', 'rollout_filter_1600_count_opt.csv', 'rollout_filter_2000_count_opt.csv'
+    'rollout_filter_1200.csv', 'rollout_filter_1600.csv', 'rollout_filter_2000.csv'
 ]
 
 labels = [
@@ -16,8 +16,12 @@ labels = [
 # Read data
 for file in file_names:
     df = pd.read_csv('./data/experiment/' + file)
-    comp_time_list.append(df['comp_time'])
-    opt_list.append(df['num_opts'])
+    df['comp_time'] = pd.to_numeric(df['comp_time'], errors='coerce')
+    df['num_opts'] = pd.to_numeric(df['num_opts'], errors='coerce')
+    comp_time_list.append(df['comp_time'].dropna())
+    opt_list.append(df['num_opts'].dropna())
+
+print(opt_list)
 
 # --- Plot setup ---
 fig, axs = plt.subplots(2, 1, figsize=(10, 8))
@@ -31,18 +35,18 @@ def annotate_medians(ax, data_list):
                 va='center', ha='left', fontsize=14, color='black')
 
 # --- Computation time boxplot ---
-axs[0].boxplot(comp_time_list, labels=labels, patch_artist=True, sym='')
+axs[0].boxplot(comp_time_list, labels=labels, patch_artist=True, sym='', showfliers=True)
 axs[0].set_title("Computation Time for MPPI Without and With Safety-Filtered Rollouts")
 axs[0].set_ylabel("Time (s)")
 axs[0].grid(True, axis='y', linestyle='--', alpha=0.6)
 annotate_medians(axs[0], comp_time_list)
 
 # --- Distance boxplot ---
-axs[1].boxplot(opt_list, labels=labels, patch_artist=True, sym='')
+axs[1].boxplot(opt_list, labels=labels, patch_artist=True, showfliers=True)
 axs[1].set_title("Number of Rollout Optimization Problems per Step")
 axs[1].set_ylabel("# Optimizations per Rollout")
 axs[1].grid(True, axis='y', linestyle='--', alpha=0.6)
 annotate_medians(axs[1], opt_list)
 
 plt.tight_layout()
-plt.savefig("./data/experiment/number_opts.png", dpi=300, bbox_inches='tight')
+plt.savefig("./data/experiment/number_opts1.png", dpi=300, bbox_inches='tight')
